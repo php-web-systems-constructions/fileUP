9012201f151f206bba523e4b7262f2846cd06325
KaminekLP <30894874+KaminekLP@users.noreply.github.com>
2017-11-07 16:00:33 +0100

    v1.2

--- index.php
+++ index.php
@@ -6,21 +6,39 @@
 /* Include config */
 include "core/config.php";
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title><?= $dTitle ?></title>
-<link rel="stylesheet" href="core/main.css">
-</head>
-<body>
-<div class="content">
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    <div class="text"><?= $messageInfoChoose ?></div><br>
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <br><br><input type="submit" value="<?= $messageInfoButton ?>" name="submit" class="button">
-</form>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <?= $dTitle ?>
+    </title>
+    <link rel="stylesheet" href="core/main.css">
+  </head>
+  <body>
+    <div class="content">
+      <form action="upload.php" method="post" enctype="multipart/form-data">    
+        <div class="text">
+          <?= $messageInfoChoose ?>
+        </div>
+        <br>    
+        <input type="file" name="fileToUpload" id="fileToUpload">    
+        <br>
+        <br>
+        <input type="submit" value="<?= $messageInfoButton ?>" name="submit" class="button">
+      </form>
+    </div>
+    
+<footer class="footer">
+<hr>
+<!-- DO NOT REMOVE FOLLOWING TEXT, IF YOU HAVENT GOT PAID LICENSE! IF YOU REMOVE THIS, YOU WILL BY VERY IT RISK!!! --> 
+<div class="copy">
+Powered by <a href="https://github.com/kamineklp/fileup" class="href" target="_blank">fileUP!</a> created by KaminekLP
 </div>
-</body>
+<!-- DO NOT REMOVE TO HERE -->
+<div class="owntext">
+<?php echo $messageInfoOwnCopy; ?>
+</div>
+</footer>
+  </body>
 </html>
