7036430716546b317c8db923c9b7163ff050512b
KaminekLP <30894874+KaminekLP@users.noreply.github.com>
2017-11-07 16:00:55 +0100

    v1.2

--- upload.php
+++ upload.php
@@ -9,12 +9,15 @@ include "core/config.php";
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title><?= $dTitle ?></title>
-<link rel="stylesheet" href="core/uploadCss.css">
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <?= $dTitle ?>
+    </title>
+    <link rel="stylesheet" href="core/uploadCss.css">
+    <link rel="stylesheet" href="core/main.css">
+  </head>
+  <body>
 <?php
 /* Sets evrything is needed */
 $target_dir = $fDir;
@@ -38,12 +41,14 @@ if(isset($_POST["submit"])) {
 
 /* Check if file already exists */
 if (file_exists($target_file)) {
-    echo "<div class='podChyba'>". $messageErrAlredyExist . "</div>";
+    echo "<div class='subError'>". $messageErrAlredyExist . "</div>";
     $uploadOk = 0;
 }
+
+
 /* Check file size - adjustable in config */
-if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "<div class='podChyba'>" . $messageErrLargeSize . "</div>";
+if ($_FILES["fileToUpload"]["size"] > 50000000) {
+    echo "<div class='subError'>" . $messageErrLargeSize . "</div>";
     $uploadOk = 0;
 }
 
@@ -53,24 +58,36 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 && $imageFileType != "gif" && $imageFileType != "mp4" && $imageFileType != "avi" 
 && $imageFileType != "txt" && $imageFileType != "zip" && $imageFileType != "rar" 
 && $imageFileType != "tar.gz" ) {
-    echo "<div class='podChyba'>" . $messageErrNoAllowedFormat . "</div>";
+    echo "<div class='subError'>" . $messageErrNoAllowedFormat . "</div>";
     $uploadOk = 0;
 }
 
 
 /* Check if all ok */
 if ($uploadOk == 0) {
-    echo "<div class='error'><div class='nadpisErr'>" . $messageErrTitle . "</div>" . $messageErrFileUploadError1 . "<br>" . $messageErrFileUploadError2 . "</div>";
+    echo "<div class='error'><div class='titleErr'>" . $messageErrTitle . "</div>" . $messageErrFileUploadError1 . "<br>" . $messageErrFileUploadError2 . "</div>";
     
     
 /* If everything is ok, try to upload file */
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "<div class='ok'><div class='nadpisOk'>" . $messageOkTitle . "</div>" . $messageOkUploaded . "<br><pre>" . $homeUrl . "" . $fDir . "". basename( $_FILES['fileToUpload']['name']);
+        echo "<div class='ok'><div class='titleOk'>" . $messageOkTitle . "</div>" . $messageOkUploaded . "<br><pre>" . $homeUrl . "" . $fDir . "". basename( $_FILES['fileToUpload']['name']);
     } else {
         echo "<div class='error'>" . $messageErrFatal . "</div>";
     }
 }
-?>
-</body>
+
+    ?>
+    <footer class="footer">
+<hr>
+<!-- DO NOT REMOVE FOLLOWING TEXT, IF YOU HAVENT GOT PAID LICENSE! IF YOU REMOVE THIS, YOU WILL BY VERY IT RISK!!! --> 
+<div class="copy">
+Powered by <a href="https://github.com/kamineklp/fileup" class="href" target="_blank">fileUP!</a> created by KaminekLP
+</div>
+<!-- DO NOT REMOVE TO HERE -->
+<div class="owntext">
+<?php echo $messageInfoOwnCopy; ?>
+</div>
+</footer>
+  </body>
 </html>
